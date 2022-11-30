7b99b6cc071dbcdc4b59eace5d3dd8fe56b693f3
d.bruckner <d.bruckner@htlkrems.at>
2022-11-30 11:55:34 +0100

    update

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h2>Project list (all designs from Frontend mentor)</h2>
+      <a href='https://thriving-jelly-e48a9c.netlify.app/'>E-Commerce</a><br/>
+      <a href='https://stellular-donut-c23b28.netlify.app//'>Quiz-App</a><br/>
+      <a href='https://super-horse-a074f4.netlify.app'>Chart-Component</a>
+    </div>
+  )
+}
+
+export default App
