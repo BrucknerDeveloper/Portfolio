6672b6171a1fff63c8ca2936d9e56b40a79a79f0
Daniel <bruckner.developer@gmail.com>
2023-02-12 19:20:50 +0100

    continued MyWork section, added default exports to deploy sit
    About Me and Contact need to have a default React setup so netlify can build it and deploy.

--- src/components/Main/MyWork/MyWork.tsx
+++ src/components/Main/MyWork/MyWork.tsx
@@ -3,10 +3,17 @@ import React from "react"
 import Project from "./Project"
 
 import project1 from "/src/assets/arrow-up.svg"
+import githubsvg from "/src/assets/github.svg"
 
 export default function() {
-    return <div>
+    return <section id="MyWork" className="work">
         <h2 className="primary-heading fs-h2">My Work</h2>
-        {/* <Project img={project1} /> */}
-    </div>
+        <Project 
+            preview={githubsvg} 
+            title="Quizzical App"
+            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, eius." 
+            technologies={["HTML", "SCSS", "Javascript", "React"]}
+            link={"https://www.youtube.com"}
+        />
+    </section>
 }
