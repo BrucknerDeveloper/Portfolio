25e48d2ec97e82eaeddcd6164bcdbfb5a1ab0145
Daniel <bruckner.developer@gmail.com>
2023-02-14 20:19:04 +0100

    IGNORE

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,7 +4,6 @@ import github_logo from "/src/assets/github.svg"
 import linkedIn_logo from "/src/assets/linkedIn.svg"
 import frontEndMentor_logo from "/src/assets/frontendmentor.png"
 
-
 export default function() {
     return <footer className="footer">
         <a href="#"><img className="footer__icon" src={github_logo} /></a>
