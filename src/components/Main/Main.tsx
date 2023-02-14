26d240ff6ebe790a63baefd010f84c6b412bb089
Daniel <bruckner.developer@gmail.com>
2023-02-14 19:29:27 +0100

    AboutMe finished (mobile)

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,9 +1,11 @@
 import React from "react"
 
 import MyWork from "./MyWork/MyWork"
+import AboutMe from "./AboutMe"
 
 export default function() {
     return <main className="main">
         <MyWork />
+        <AboutMe />
     </main>
 }
