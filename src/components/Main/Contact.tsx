15ee62c155b3a2223ec26499a69856d50e66ffc3
Daniel <bruckner.developer@gmail.com>
2023-02-14 19:41:03 +0100

    contact finished

--- src/components/Main/Contact.tsx
+++ src/components/Main/Contact.tsx
@@ -2,7 +2,14 @@ import React from "react"
 
 
 export default function() {
-    return <section>
-        <p>Contact</p>
+    return <section id="Contact" className="contact">
+        <div className="gutter-row">
+            <h2 className="primary-heading">Get in Touch</h2>
+            <p className="contact__description fs-normal white-1">Are you looking for a fast-performing and user-friendly website to represent your product or business? or 
+                looking for any kind of consultation? or want to ask questions? or have some advice for me or just want to 
+                say "Hi 👋" in any case feel free to Let me know. I will do my best to respond back. 😊 The quickest way 
+                to reach out to me is via an email.</p>
+            <a href="mailto:bruckner.developer@gmail.com" className="btn btn--left-to-right-hover fs-normal">bruckner.developer.@gmail.com</a>
+        </div>    
     </section>
 }
--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -2,10 +2,12 @@ import React from "react"
 
 import MyWork from "./MyWork/MyWork"
 import AboutMe from "./AboutMe"
+import Contact from "./Contact"
 
 export default function() {
     return <main className="main">
         <MyWork />
         <AboutMe />
+        <Contact />
     </main>
 }
