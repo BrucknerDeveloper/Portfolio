23e32ae370c353c6d498028604954a010b7993d8
Daniel <bruckner.developer@gmail.com>
2023-02-14 20:15:12 +0100

    started footer
    new great figma plugin (remove BG)

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,14 @@
 import React from "react"
 
+import github_logo from "/src/assets/github.svg"
+import linkedIn_logo from "/src/assets/linkedIn.svg"
+import frontEndMentor_logo from "/src/assets/frontendmentor.png"
+
+
 export default function() {
-    return <footer>
-        <p>footer</p>
+    return <footer className="footer">
+        <a href="#"><img className="footer__icon" src={github_logo} /></a>
+        <a href="#"><img className="footer__icon" src={linkedIn_logo} /></a>
+        <a href="#"><img className="footer__icon" src={frontEndMentor_logo} /></a>
     </footer>
 }
