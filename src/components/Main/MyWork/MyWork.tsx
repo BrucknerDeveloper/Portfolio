42b1d20cd710dc3128ee21e419de8c57536a2552
Daniel <bruckner.developer@gmail.com>
2023-02-14 18:30:37 +0100

    MyWork section finished (mobile)

--- src/components/Main/MyWork/MyWork.tsx
+++ src/components/Main/MyWork/MyWork.tsx
@@ -7,30 +7,32 @@ import project_url from "/src/assets/Project_Url-shortening.png"
 import project_sneaker from "/src/assets/Project_Sneaker.png"
 
 export default function() {
-    return <section id="MyWork" className="work">
-        <h2 className="primary-heading fs-h2">My Work</h2>
-        <Project 
-            preview={project_url} 
-            title="Quizzical App"
-            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, eius." 
-            technologies={["HTML", "SCSS", "Javascript", "React"]}
-            link={"https://www.youtube.com"}
-        />
+    return <section id="MyWork" className="myWork">
+        <div className="gutter-row">
+            <h2 className="primary-heading fs-h2">My Work</h2>
+            <Project 
+                preview={project_url} 
+                title="Quizzical App"
+                description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, eius." 
+                technologies={["HTML", "SCSS", "Javascript", "React"]}
+                link={"https://www.youtube.com"}
+            />
 
-        <Project 
-            preview={project_sneaker} 
-            title="Quizzical App"
-            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, eius." 
-            technologies={["HTML", "SCSS", "Javascript", "React"]}
-            link={"https://www.youtube.com"}
-        />
+            <Project 
+                preview={project_sneaker} 
+                title="Quizzical App"
+                description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, eius." 
+                technologies={["HTML", "SCSS", "Javascript", "React"]}
+                link={"https://www.youtube.com"}
+            />
 
-        <Project 
-            preview={project_quiz} 
-            title="Quizzical App"
-            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, eius." 
-            technologies={["HTML", "SCSS", "Javascript", "React"]}
-            link={"https://www.youtube.com"}
-        />
+            <Project 
+                preview={project_quiz} 
+                title="Quizzical App"
+                description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, eius." 
+                technologies={["HTML", "SCSS", "Javascript", "React"]}
+                link={"https://www.youtube.com"}
+            />
+        </div>
     </section>
 }
