aafba2594f0c49039b429085f0fd3ea773da8031
Daniel <brucki2002@gmail.com>
2023-02-17 23:12:46 +0100

    updated variables, media queries for variales and gutters
    started header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import React from "react"
 export default function() {
     return <header className="header" id="header">
         {/* Navigation */}
-        <nav className="nav fs-normal white-1">
+        <nav className="nav fs-normal white-1 gutter-row">
             <ul className="nav__items">
                 <li className="nav__item"><a href="#myWork" className="nav__link">Work</a></li>
                 <li className="nav__item"><a href="#aboutMe" className="nav__link">About</a></li>
@@ -15,7 +15,7 @@ export default function() {
         <div className="header__intro">
             <h1 className="header__title fs-h1">Daniel Bruckner</h1>
             <p className="header__description fs-medium ff-jost white-1">A Front-end Developer based in Vienna, AT</p>
-            <a href="#contact" className="btn btn--pink fs-medium">Get in touch</a>
+            <a href="#contact" className="btn btn--pink fs-normal">Get in touch</a>
         </div>
     </header>
 } 
