7f62e355b437a065a33d8aba340b52acc2fb306a
Daniel <brucki2002@gmail.com>
2023-02-19 00:32:51 +0100

    still

--- src/components/Main/AboutMe.tsx
+++ src/components/Main/AboutMe.tsx
@@ -14,7 +14,7 @@ export default function() {
                         real-world problems, and that are delightful to use. My specialties include HTML, CSS/SCSS, TypeScript, 
                         React JS, and animations (GSAP, AnimeJS)</p>
                     <p className="aboutMe__description">I went to school in HTL Krems with a primary focus on media technology.</p>
-                    <a href="/src/assets/Bruckner_Lebenslauf.pdf" className="aboutMe__btn btn btn--left-to-right-hover">My Resume</a>           
+                    <a href={lebenslauf} className="aboutMe__btn btn btn--left-to-right-hover">My Resume</a>           
                     <iframe src={lebenslauf} height="200" width="300"></iframe>
                 </div>
             </div>     
