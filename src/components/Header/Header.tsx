6b7cc2f4358314c4f27ef358b515c5e292759821
Daniel <brucki2002@gmail.com>
2023-02-19 20:41:04 +0100

    imported gsap, learning to use it with react

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,18 @@
-import React from "react"
+import React, { useLayoutEffect, useRef } from "react";
+import { gsap } from "gsap";
 
 export default function() {
+    const introRef = useRef<any>();
+
+    useLayoutEffect(() => {
+        // Refs allow you to access DOM nodes
+        console.log(introRef) // { current: div.box }
+        // then we can animate them like so...
+        gsap.to(introRef.current, {
+          rotation: "+=360"
+        });
+      });
+
     return <header className="header" id="header">
         {/* Navigation */}
         <nav className="nav fs-normal white-1 gutter-row">
@@ -13,7 +25,7 @@ export default function() {
 
         {/* Intro */}
         <div className="gutter-row header__row">
-            <div className="header__intro" style={{position: "relative"}}>
+            <div className="header__intro" ref={introRef}>
                 <h1 className="header__title fs-h1">Daniel Bruckner</h1>
                 <p className="header__description fs-medium ff-jost white-1">A Front-end Developer based in Vienna, AT</p>
                 <a href="#contact" className="btn btn--pink fs-normal">Get in touch</a>
