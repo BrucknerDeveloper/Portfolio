9e256330c886fa85d4b0a3c8bd062c53f871ac94
Daniel <brucki2002@gmail.com>
2023-03-28 17:47:07 +0200

    fixed email typo

--- src/components/Main/Contact.tsx
+++ src/components/Main/Contact.tsx
@@ -43,7 +43,7 @@ export default function() {
             <p className="contact__description fs-small white-1">Are you looking for a young, motivated web developer? Who can work in a team? 
             Who likes to learn new things and deliver stunning websites? Feel free to let me know. I will do my best to respond back. 😊 The quickest way 
                 to reach out to me is via an email.</p>
-            <a href="mailto:bruckner.developer@gmail.com" className="contact--btn btn btn--left-to-right-hover fs-small">bruckner.developer.@gmail.com</a>
+            <a href="mailto:bruckner.developer@gmail.com" className="contact--btn btn btn--left-to-right-hover fs-small">bruckner.developer@gmail.com</a>
         </div>    
     </section>
 }
